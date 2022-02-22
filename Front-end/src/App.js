cd40c5621592b12b009ccdfa56e671e2e337a698
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-02-22 18:29:45 +0100

    Add files via upload

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NavBar from "./Components/Header/NavBar";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <React.Fragment>
+      <NavBar />
       <Footer />
     </React.Fragment>
   );
