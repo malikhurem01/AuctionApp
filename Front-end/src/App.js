89bfcc9865b13e0b257fcc58eeb32dd897db77e6
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-02-22 09:48:38 +0100

    Implemented Footer

---
+++ Front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
