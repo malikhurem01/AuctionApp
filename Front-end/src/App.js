18be720026ace833480902f8f88b257fe7a46070
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-02-24 16:37:15 +0100

    Add files via upload

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,27 +1,11 @@
 import React from "react";
 
-import { Route } from "react-router-dom";
 import PageLayoutWrapper from "./Components/PageLayoutWrapper";
 
-import PrivacyAndPolicy from "./Pages/PrivacyAndPolicy";
-import TermsAndConditions from "./Pages/TermsAndConditions";
-import AboutUs from "./Pages/AboutUs";
-
 //isloggedIn props will later be populated with the correct value, false is just a dummy value as for the initial project
+//header and footer are contained inside the PageLayoutWrapper component
 function App() {
-  return (
-    <PageLayoutWrapper isLoggedIn={false}>
-      <Route path="/privacy-and-policy">
-        <PrivacyAndPolicy />
-      </Route>
-      <Route path="/terms-and-conditions">
-        <TermsAndConditions />
-      </Route>
-      <Route path="/about-us">
-        <AboutUs />
-      </Route>
-    </PageLayoutWrapper>
-  );
+  return <PageLayoutWrapper isLoggedIn={false}></PageLayoutWrapper>;
 }
 
 export default App;
