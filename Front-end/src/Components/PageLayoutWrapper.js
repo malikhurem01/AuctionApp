a0cd0056f352023aba8fa5614b7422d984e43cf6
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-02-24 16:40:50 +0100

    Corrected files

--- Front-end/src/Components/PageLayoutWrapper.js
+++ Front-end/src/Components/PageLayoutWrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 
-import classes from "./style.module.css";
+import classes from "./PageLayoutWrapper.module.css";
 
 const PageLayoutWrapper = (props) => {
   return (
