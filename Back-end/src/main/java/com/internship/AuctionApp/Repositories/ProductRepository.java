997a97bd345943cc149ae3763df0fac4fb47dbe0
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-03-08 22:05:08 +0100

    Delete Back-end directory

--- Back-end/src/main/java/com/internship/AuctionApp/Repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.internship.AuctionApp.Repositories;
-
-import com.internship.AuctionApp.Models.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
