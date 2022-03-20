9ea86fbd2bf282e32ae69a5abe092f4468f04133
Malik <malikhuremovic01@gmail.com>
2022-03-21 00:11:18 +0100

    Custom exceptions

---
+++ Back-end/src/main/java/com/internship/AuctionApp/Exceptions/UserExistsException.java
@@ -0,0 +1,10 @@
+package com.internship.AuctionApp.Exceptions;
+
+public class UserExistsException extends RuntimeException{
+    public UserExistsException(){
+        super("User with that email already exists");
+    }
+    public UserExistsException(String message){
+        super(message);
+    }
+}
