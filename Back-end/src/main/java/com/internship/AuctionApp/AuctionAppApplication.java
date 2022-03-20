3b63ba752613f159af31d081556e5dbbf6911433
Malik <malikhuremovic01@gmail.com>
2022-03-21 00:08:59 +0100

    Main file slightly updated

--- Back-end/src/main/java/com/internship/AuctionApp/AuctionAppApplication.java
+++ Back-end/src/main/java/com/internship/AuctionApp/AuctionAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuctionAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuctionAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionAppApplication.class, args);
+    }
 }
